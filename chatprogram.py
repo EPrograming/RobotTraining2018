dbf01944d5ae6bd2ef1f2f06d3051b00667ce1d4
Eli <ethds24@gmail.com>
2017-10-22 18:56:13 -0400

    update

--- chatprogram.py
+++ chatprogram.py
@@ -1,24 +1,42 @@
+
+import socket
+HOST = ''                 # Symbolic name meaning all available interfaces
+PORT = 8000              # Arbitrary non-privileged port
 #Connect to the other computer
-def connect(conn):
-	print(conn)
-	return
+def connect():
+	textToSend = input("What do you want to send?")
+	b = textToSend.encode('utf-8')
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+	    s.bind((HOST, PORT))
+	    s.listen(1)
+	    conn, addr = s.accept()
+	    with conn:
+	        print('Connected by', addr)
+	        while True:
+	            data = conn.recv(1024)
+	            if not data: break
+
+	            conn.sendall(b)
+	
+	
 
 
 #Send text
-def Send(sendtext):
-	print(sendtext)
-	return
+
+
+
 def sendingstuff():
 
-	ip = input("What is the other computer's ip address?")
-	connect(ip)
+	#ip = input("What is the other computer's ip address?")
+	#ipint = int(ip)
+	
 
 
 
-
-	textToSend = input("What do you want to send?")
-
-	Send(textToSend)
+	
+	
+	connect()
+	
 
 def main():
 	print("Welcome to my simple chat program")
